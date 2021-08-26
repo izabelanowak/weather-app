0a7fb7b006c139ed01fd58c575f89974060a31cb
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-26 22:30:55 +0200

    add types of tiles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ function App() {
         type="humidity"
         value="49%"
       />
+      <Tile
+        type="pressure"
+        value="1,009mBar"
+      />
+      <Tile
+        type="wind"
+        value="11km/h"
+      />
+      <Tile
+        type="sunrise"
+        value="6:03 AM"
+      />
+      <Tile
+        type="sunset"
+        value="7:05 PM"
+      />
+      <Tile
+        type="daytime"
+        value="13h 1m"
+      />
     </>
   );
 }
--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -1,14 +1,40 @@
 import { Label, Value, Wrapper } from "./styled";
 import { ReactComponent as HumidityIcon } from "../../assets/humidity.svg";
+import { ReactComponent as PressureIcon } from "../../assets/barometer.svg";
+import { ReactComponent as WindIcon } from "../../assets/wind.svg";
+import { ReactComponent as SunriseIcon } from "../../assets/sunrise.svg";
+import { ReactComponent as SunsetIcon } from "../../assets/sunset.svg";
+import { ReactComponent as DaytimeIcon } from "../../assets/sand-clock.svg";
 
 export const Tile = ({ type, value }) => {
   let icon;
   let labelText;
-  
+
   switch (type) {
     case "humidity":
       icon = <HumidityIcon />
       labelText = "Humidity";
+      break;
+    case "pressure":
+      icon = <PressureIcon />
+      labelText = "Pressure";
+      break;
+    case "wind":
+      icon = <WindIcon />
+      labelText = "Wind";
+      break;
+    case "sunrise":
+      icon = <SunriseIcon />
+      labelText = "Sunrise";
+      break;
+    case "sunset":
+      icon = <SunsetIcon />
+      labelText = "sunset";
+      break;
+    case "daytime":
+      icon = <DaytimeIcon />
+      labelText = "Daytime";
+      break;
   }
   return (
     <Wrapper>
