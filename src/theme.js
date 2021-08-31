7c3be51b456b39a6b4e4ab471fcd3c795e742934
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-31 12:36:09 +0200

    add new colors

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,9 @@ export const theme = {
     colors: {
         white: "#FFFFFF",
         dustyGray: "#999999",
+        doveGray: "#666666",
         tundora: "#444444",
+        black: "#000000",
     },
     breakpoints: {
         mobile: "577",
